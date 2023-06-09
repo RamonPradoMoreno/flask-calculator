07c8fed69a98110a5d02df46883e6c961e16105e
Ramón Prado Moreno <ramon.prado@erudit.ai>
2023-06-09 14:51:59 +0200

    feat: Improvements in calculator thanks to testing and better tests.

--- flaskr/calculator.py
+++ flaskr/calculator.py
@@ -1,4 +1,10 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, abort
+from decimal import Decimal, getcontext, InvalidOperation
+
+# Set the maximum precision to x digits
+# This is important due to how floating point works in Python
+# Try doing 1.1 + 2.1 in the console
+getcontext().prec = 12
 
 bp = Blueprint("calculator", __name__, url_prefix="/calculator")
 
@@ -7,8 +13,14 @@ bp = Blueprint("calculator", __name__, url_prefix="/calculator")
 def add():
     try:
         request_body = request.get_json()
-        left_operand = request_body.get("left_operand")
-        right_operand = request_body.get("right_operand")
+        # Decimal has been used to avoid float rounding problems
+        left_operand = Decimal(request_body.get("left_operand"))
+        right_operand = Decimal(request_body.get("right_operand"))
+
         return {"result": left_operand + right_operand}
-    except Exception as error:
+    except TypeError as error:
         abort(code=400, description="Missing some operands")
+    except InvalidOperation as error:
+        abort(code=400, description="Operands are not numbers")
+    except Exception as error:
+        abort(code=500, description="Unknown error")
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,13 +1,71 @@
 import pytest
 from flask import g, session
 from flaskr.db import get_db
+from decimal import Decimal
 
 
-def test_add(client, app):
-    assert (
-        client.post(
-            path="/calculator/add",
-            json={"left_operand": 1, "right_operand": 2},
-        ).status_code
-        == 200
+def test_returns_200(client, app):
+    response = client.post(
+        path="/calculator/add",
+        json={"left_operand": 1, "right_operand": 2},
     )
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize(
+    ("left_operand", "right_operand", "message"),
+    (
+        (None, None, b"Missing some operands"),
+        (1, None, b"Missing some operands"),
+        (None, 1, b"Missing some operands"),
+    ),
+)
+def test_add_validate_input_not_empty(client, left_operand, right_operand, message):
+    response = client.post(
+        path="/calculator/add",
+        json={
+            "left_operand": left_operand,
+            "right_operand": right_operand,
+        },
+    )
+    assert message in response.data
+
+
+@pytest.mark.parametrize(
+    ("left_operand", "right_operand", "message"),
+    (
+        ("hello", "1", b"Operands are not numbers"),
+        ("1", "hello", b"Operands are not numbers"),
+        ("1,1", "1,1", b"Operands are not numbers"),
+    ),
+)
+def test_add_validate_input_number(client, left_operand, right_operand, message):
+    response = client.post(
+        path="/calculator/add",
+        json={
+            "left_operand": left_operand,
+            "right_operand": right_operand,
+        },
+    )
+    assert message in response.data
+
+
+@pytest.mark.parametrize(
+    ("left_operand", "right_operand", "result"),
+    (
+        (1, 1, "2"),
+        (1.1, 2.2, "3.3"),
+        (1, -1, "0"),
+        (-1, 1, "0"),
+        (-1, -1, "-2"),
+    ),
+)
+def test_add_works(client, left_operand, right_operand, result):
+    response = client.post(
+        path="/calculator/add",
+        json={
+            "left_operand": left_operand,
+            "right_operand": right_operand,
+        },
+    )
+    assert Decimal(response.json.get("result")) == Decimal(result)
